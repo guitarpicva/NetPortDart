49b2fb9313dc69057d79f090329a4e6dec65ec7a
Mitchell Winkle <guitarpicva@users.noreply.github.com>
2025-02-16 09:38:48 -0500

    remove comment

--- netport.dart
+++ netport.dart
@@ -7,37 +7,40 @@ import 'dart:typed_data';
 late SerialPort _modem;
 late ServerSocket _ss;
 late Socket _tcp;
+String _port = '19798'; // default
 String serBuffer = '';
-bool b_NetConnected = false;
+bool bNetConnected = false;
 void main(List<String> arguments) {
-  print('NetPort!');
-  var port = '19798'; // default
+  //print('NetPort!');
+  
   /// create the socket/serial to the modem/controller and set up handlers
   
-  String serial = 'ttyACM0'; // default for Harris radio
+  var serial = 'ttyACM0'; // default for radio
   if(arguments.isNotEmpty) {
     serial = arguments.first;
   }
   if(arguments.length > 1) {
-    port = arguments.elementAt(1);
+    _port = arguments.elementAt(1);
+    //print("port: $_port");
   }
-  startTcpServer(int.parse(port));  
+  startTcpServer(int.parse(_port));  
   getModem(serial);
 }
 
 /// Start the server process to listen on the any
-  /// ip address.
-  /// Automatically starts the client socket handler upon
-  /// new connection (one connection only)
-  Future<ServerSocket> startTcpServer(int port) async {
-    var ss =
-        await ServerSocket.bind(InternetAddress.anyIPv4, port, shared: true);
-    _ss = ss;
-    ss.listen((client) {
-      getTcp(client);
-    });
-    return ss;
-  }
+/// ip address.
+/// Automatically starts the client socket handler upon
+/// new connection (one connection only)
+Future<ServerSocket> startTcpServer(int port) async {
+  var ss =
+      await ServerSocket.bind(InternetAddress.anyIPv4, port, shared: true);
+  _ss = ss;
+  _ss.listen((client) {
+    getTcp(client);
+  });
+  print('Server Socket started on port: $port');
+  return ss;
+}
 
 Future<void> getModem(String address) async {
     try {
@@ -50,15 +53,16 @@ Future<void> getModem(String address) async {
       spc.stopBits = 1;
       spc.setFlowControl(SerialPortFlowControl.none);
       if (Platform.isLinux || Platform.isMacOS) {
-        print('Linux Radio:$address');
-        _modem = SerialPort('/dev/$address');
+        print('Linux Radio: $address');
+        _modem = SerialPort('/dev/$address'); // i.e. ttyACM0
         open = _modem.openReadWrite();
         _modem.config = spc;        
       } else {
         // essentially Windows is the only other viable candidate ATM
-        print('Windows Radio:$address');
+        print('Windows Radio: $address');
         _modem = SerialPort(address); // i.e. COM23
-        open = _modem.openReadWrite();        
+        open = _modem.openReadWrite();   
+        spc.dtr = 1; // Windows is weird
         _modem.config = spc;        
       }      
       if (open) {
@@ -83,7 +87,7 @@ Future<void> getModem(String address) async {
 /// Write Serial port data to the TCP Socket
 Future<void> handleSerialPortData(Uint8List lines) async {
   print(String.fromCharCodes(lines));
-  if(b_NetConnected) {
+  if(bNetConnected) {
     _tcp.write(String.fromCharCodes(lines)); // for String data
     // or _tcp.write(lines); // for binary data
     await _tcp.flush();
@@ -102,7 +106,7 @@ Future<void> handleTCPPortData(Uint8List lines) async {
 void getTcp(Socket client) {
     _tcp = client;
     _tcp.setOption(SocketOption.tcpNoDelay, true);
-    b_NetConnected = true;
+    bNetConnected = true;
     //String controlBuffer = '';
     print('Control client connected...');
     client.listen((Uint8List data) async {
@@ -115,6 +119,6 @@ void getTcp(Socket client) {
     onDone: () {
       print('Control client finished...');
       client.close();
-      b_NetConnected = false;
+      bNetConnected = false;
     });
   }
