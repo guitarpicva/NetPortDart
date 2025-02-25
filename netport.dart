dc6e1e921a16071568ed68cd0eb3203d850ce215
Mitchell Winkle <guitarpicva@users.noreply.github.com>
2025-02-25 11:46:28 -0500

    Add files via upload

--- netport.dart
+++ netport.dart
@@ -18,19 +18,28 @@ void main(List<String> arguments) {
     serial = arguments.first;
     //print("serial:$serial");
   }
+  // else { 
+  // Derive the serial port by type/manufacturer and enumeration.
+  // If this fails use the default ttyACM0
+  //}
   var port = '19798'; // default
   if(arguments.length > 1) {
     port = arguments.elementAt(1);
     //print("port: $_port");
-  }
+  }  
   startTcpServer(int.parse(port));  
-  getModem(serial);
+  getSerial(serial);
 }
 
-/// Start the server process to listen on the any
-/// ip address.
+/// Start the server process to listen on the any ip address.
 /// Automatically starts the client socket handler upon
-/// new connection (one connection only)
+/// new connection (one connection only).
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// Consider limiting this to the Docker IP space if
+// used for Docker.  NetPort would live on the host
+// machine, in order to link the ASCII port into
+// a running container via a socket.
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 Future<ServerSocket> startTcpServer(int port) async {
   var ss =
       await ServerSocket.bind(InternetAddress.anyIPv4, port, shared: true);
@@ -42,50 +51,50 @@ Future<ServerSocket> startTcpServer(int port) async {
   return ss;
 }
 
-Future<void> getModem(String address) async {
-    try {
-      bool open = false;
-      var spc = SerialPortConfig();
-      // seems to work fine at this speed, but slower also works
-      spc.baudRate = 115200; 
-      spc.bits = 8;
-      spc.parity = 0;
-      spc.stopBits = 1;
-      spc.setFlowControl(SerialPortFlowControl.none);
-      if (Platform.isLinux || Platform.isMacOS) {
-        print('Linux Radio: $address');
-        if(address.startsWith("/dev/")) {
-          address = address.substring(5);
-        }
-        _modem = SerialPort('/dev/$address'); // i.e. ttyACM0
-        open = _modem.openReadWrite();
-        _modem.config = spc;        
-      } else {
-        // essentially Windows is the only other viable candidate ATM
-        print('Windows Radio: $address');
-        _modem = SerialPort(address); // i.e. COM23
-        open = _modem.openReadWrite();   
-        spc.dtr = 1; // Windows is weird
-        _modem.config = spc;        
-      }      
-      if (open) {
-        print("$address: OPEN!");
-        final reader = SerialPortReader(_modem);
-        reader.stream.listen((data) {
-          handleSerialPortData(data);
-        });
-      } 
-      else {
-        print("$address: NOT OPEN!");
-        _modem.dispose();
+Future<void> getSerial(String address) async {
+  try {
+    bool open = false;
+    var spc = SerialPortConfig();
+    // seems to work fine at this speed, but slower also works
+    spc.baudRate = 115200; 
+    spc.bits = 8;
+    spc.parity = 0;
+    spc.stopBits = 1;
+    spc.setFlowControl(SerialPortFlowControl.none);
+    if (Platform.isLinux || Platform.isMacOS) {
+      print('Linux Radio: $address');
+      if(address.startsWith("/dev/")) {
+        address = address.substring(5);
       }
-    } catch (se) {
-      // connection to radio failed, so
-      // tell the UI to open the configuration Drawer
-      print('SerialException: ${se.toString()} addr:$address');      
+      _modem = SerialPort('/dev/$address'); // i.e. ttyACM0
+      open = _modem.openReadWrite();
+      _modem.config = spc;        
+    } else {
+      // essentially Windows is the only other viable candidate ATM
+      print('Windows Radio: $address');
+      _modem = SerialPort(address); // i.e. COM23
+      open = _modem.openReadWrite();   
+      spc.dtr = 1; // Windows is weird
+      _modem.config = spc;        
+    }      
+    if (open) {
+      print("$address: OPEN!");
+      final reader = SerialPortReader(_modem);
+      reader.stream.listen((data) {
+        handleSerialPortData(data);
+      });
+    } 
+    else {
+      print("$address: NOT OPEN!");
+      _modem.dispose();
     }
-    return; // _modem;
+  } catch (se) {
+    // connection to radio failed, so
+    // tell the UI to open the configuration Drawer
+    print('SerialException: ${se.toString()} addr:$address');      
   }
+  return; // _modem;
+}
 
 /// Write Serial port data to the TCP Socket
 Future<void> handleSerialPortData(Uint8List lines) async {
